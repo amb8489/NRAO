f2e681ae541ce30eb047abd6efec7ca0ee5c8942
Aaron Berghash <amb8489@rit.edu>
2022-09-26 15:39:25 -0400

    stubbed out functions for conductivity equations

---
+++ SuperConductivityEquations/SCE.py
@@ -0,0 +1,82 @@
+'''
+NRAO
+Aaron Berghash amb8489@g.rit.edu
+'''
+
+
+import math
+
+
+
+
+def e1(e,delta):
+    return math.sqrt(e**2 - delta**2)
+
+
+def e2(e,delta,hf):
+    return math.sqrt( (e+hf)**2 - delta**2)
+
+def e3():
+    pass
+
+def e4():
+    pass
+
+def g():
+    pass
+
+def g2():
+    pass
+
+def fd():
+    pass
+
+def ff():
+    pass
+
+def f2():
+    pass
+
+def int1():
+    pass
+
+def int11():
+    pass
+
+def int2():
+    pass
+
+
+def sigma1NMenos():
+    pass
+
+
+def sigma1NMas():
+    pass
+
+
+def sigma2NMenos():
+    pass
+
+
+def sigma2NMas():
+    pass
+
+
+def sigma1N():
+    pass
+
+
+def sigma2N():
+    pass
+
+
+def ConductivityN():
+    pass
+
+
+
+
+
+
+
