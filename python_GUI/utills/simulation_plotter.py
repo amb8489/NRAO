2b27282e6342e9ba9d57e751576cbf93ca561e01
Aaron Berghash <amb8489@rit.edu>
2023-02-27 13:29:31 -0500

    looking start of javier code

--- python_GUI/utills/simulation_plotter.py
+++ python_GUI/utills/simulation_plotter.py
@@ -8,17 +8,20 @@ from matplotlib import pyplot as plt
 from floquet_line_model.floquet_line_builder import floquet_line_builder
 from hfss.read_hsff_file import hsff_simulate
 from python_gui.utills.utills_gui import resolution, start_frequency, end_frequency
-from utills.functions import toGHz, unfold
+from utills.functions import toGHz, beta_unfold
 
 
 # ---------------------------- unit cell inputs from paper
 
 
-def mk_plots(frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x):
+def mk_plots(frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x,
+             floquet_transmission):
     plt.close()
     # -------------------ALPHA , BETA PLOTS---------------------------------
 
     fig1, ax1 = plt.subplots()
+    fig1.suptitle('ALPHA BETAS')
+
     ax1.set_ylabel('alpha - alpha0', color='tab:red')
     ax1.plot(frequency_range, np.array(floquet_alpha) - np.array(central_line_alpha), color='tab:red')
     ax1.tick_params(axis='y', labelcolor='tab:red')
@@ -26,36 +29,46 @@ def mk_plots(frequency_range, floquet_alpha, central_line_alpha, floquet_beta, c
 
     ax2 = ax1.twinx()
     ax2.set_ylabel('beta - beta0', color='tab:green')
-    # todo for smat might need to use mk_monotinic_inc() idk
-    ax2.plot(frequency_range, np.array(unfold(floquet_beta)) - np.array(unfold(central_line_beta)), color='tab:green')
+    ax2.plot(frequency_range, np.array(beta_unfold(floquet_beta)) - np.array(beta_unfold(central_line_beta)),
+             color='tab:green')
     ax2.tick_params(axis='y', labelcolor='tab:green')
     fig1.tight_layout()
 
     # ---------------------R , X PLOTS ----------------------------
-    fig2, ax12 = plt.subplots()
+    fig2, ax22 = plt.subplots()
     color = 'tab:blue'
-    ax12.set_ylabel('r Blue -- x orange', color=color)
-    ax12.plot(frequency_range, floquet_r, color=color)
-    ax12.tick_params(axis='y', labelcolor=color)
-    ax12.set_xlabel('Frequency [GHz]')
-    ax12.plot(frequency_range, floquet_x, color='tab:orange')
+    fig2.suptitle('Floquet R and X')
+    ax22.set_ylabel('r Blue -- x orange', color=color)
+    ax22.plot(frequency_range, floquet_r, color=color)
+    ax22.tick_params(axis='y', labelcolor=color)
+    ax22.set_xlabel('Frequency [GHz]')
+    ax22.plot(frequency_range, floquet_x, color='tab:orange')
     fig2.tight_layout()
 
-    return [fig1, fig2]
+    # ---------------------Transmission PLOT ----------------------------
+    fig3, ax33 = plt.subplots()
+    color = 'tab:red'
+    fig3.suptitle('Transmission')
+    ax33.set_ylabel('Transmission', color=color)
+    ax33.plot(frequency_range, floquet_transmission, color=color)
+    ax33.tick_params(axis='y', labelcolor=color)
+    ax33.set_xlabel('Frequency [GHz]')
+    fig3.tight_layout()
+
+    return [fig1, fig2, fig3]
 
 
 def simulate(line_model):
     if line_model.type == "SMAT":
 
-        frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x = __simulate_hfss(
+        frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x, floquet_transmission = __simulate_hfss(
             line_model)
     else:
-
-        frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x = __siulate_transmission_line(
+        frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x, floquet_transmission = __siulate_transmission_line(
             line_model)
 
     return mk_plots(frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r,
-                    floquet_x)
+                    floquet_x, floquet_transmission)
 
 
 def __simulate_hfss(line_model):
@@ -70,7 +83,7 @@ def __siulate_transmission_line(line_model):
 
     # ---------------------------- storage -------------------
     floquet_alpha, floquet_beta, floquet_r, floquet_x = [], [], [], []
-    floquet_transmission_plt = []
+    floquet_transmission = []
 
     central_line_beta = []
     central_line_alpha = []
@@ -82,12 +95,13 @@ def __siulate_transmission_line(line_model):
     frequency_range = np.linspace(start_freq_GHz, end_freq_GHz, resoultion)
 
     for frequency in frequency_range:
-        alpha, beta, alphaCl, betaCL, r_, x_ = floquet_line.simulate(frequency)
+        alpha, beta, alphaCl, betaCL, r_, x_, floquet_transmission_ = floquet_line.simulate(frequency)
         central_line_beta.append(betaCL)
         central_line_alpha.append(alphaCl)
         floquet_beta.append(beta)
         floquet_alpha.append(alpha)
         floquet_r.append(r_)
         floquet_x.append(x_)
+        floquet_transmission.append(floquet_transmission_)
 
-    return frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x
+    return frequency_range, floquet_alpha, central_line_alpha, floquet_beta, central_line_beta, floquet_r, floquet_x, floquet_transmission
